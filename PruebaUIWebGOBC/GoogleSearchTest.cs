2eea183308d2eccbb48e3eb378aa15a014ce5b6a
Oivot <lukarrio350@gmail.com>
2024-10-22 10:21:44 -0600

    Agregar archivos de proyecto.

---
+++ PruebaUIWebGOBC/GoogleSearchTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Referencias 
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace PruebaUIWebGOBC
+{
+    [TestClass]
+    public class GoogleSearchTest
+    {
+        private IWebDriver driver;
+
+        public GoogleSearchTest()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+
+        public void SearchInGoogle()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            var searchBox = driver.FindElement(By.Name("q"));
+
+            searchBox.SendKeys("OpenAi");
+
+            searchBox.Submit();
+
+            System.Threading.Thread.Sleep(2000);
+
+            Assert.IsTrue(driver.Title.Contains("OpenAi"), "El titulo no contiene 'OpenAi'");
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
